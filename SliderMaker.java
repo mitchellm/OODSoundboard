d956b6355dd1c9f20fac23ce7cc2abb040186139
kruseshane <32747753+kruseshane@users.noreply.github.com>
2017-10-17 20:20:31 -0400

    Update SliderMaker.java

--- SliderMaker.java
+++ SliderMaker.java
@@ -1,12 +1,17 @@
 import javafx.scene.control.Slider;
 public class SliderMaker {
 	private S vSlider;
+	private S fSlider;
 	
 	public SliderMaker() {
 		vSlider = new VolumeSlider();
 	}
 	
-	public Slider createSlider() {
+	public Slider createVSlider() {
 		return vSlider.create();
+	}
+	
+	public Slider createFSlider() {
+		return fSlider.create();
 	}
 }
