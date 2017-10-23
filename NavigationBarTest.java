ffcadc63eb67771de06a062edf9a2f31c1959335
BrettDavis1 <32747774+BrettDavis1@users.noreply.github.com>
2017-10-23 09:13:56 -0400

    Update NavigationBarTest.java

--- NavigationBarTest.java
+++ NavigationBarTest.java
@@ -25,26 +25,77 @@ public class NavigationBarTest extends Application {
 	        MenuBar navBar = new MenuBar();
 	        
 	        //syntax to create more items to put in navbar
-	        Menu navThemes = new Menu("Themes");
+	        Menu navFile = new Menu("File");
 	        Menu navEdit = new Menu("Edit");
+	        Menu navOptions = new Menu("Options");
 	        Menu navHelp = new Menu("Help");
 	        
 	        //syntax to add options to nav tabs
 	        //MenuItem what appears in drop down when clicking Nav
 	        MenuItem about = new MenuItem("About");
+	        MenuItem open = new MenuItem("Open");
+	        MenuItem save = new MenuItem("Save");
+	        MenuItem saveAs = new MenuItem("Save As");
+	        MenuItem exit = new MenuItem("Exit");
+	        MenuItem themes = new MenuItem("Themes");
 	        
 	        //when button is clicked this happens
 	        about.setOnAction(new EventHandler<ActionEvent>() {
 	            public void handle(ActionEvent t) {
-	       
 	            	aboutInfo();
 	            }
 	        });
+	        open.setOnAction(new EventHandler<ActionEvent>() {
+	            public void handle(ActionEvent t) {
+	       
+	            	openClick();
+	            }
+				private void openClick() {
+					//
+				}
+	        });
+	        save.setOnAction(new EventHandler<ActionEvent>() {
+	            public void handle(ActionEvent t) {
+	            	saveClick();
+	            }
+
+				private void saveClick() {
+					
+				}
+	        });
+	        saveAs.setOnAction(new EventHandler<ActionEvent>() {
+	            public void handle(ActionEvent t) {
+	            	saveAsClick();
+	            }
+				private void saveAsClick() {
+					
+				}
+	        });
+	        exit.setOnAction(new EventHandler<ActionEvent>() {
+	            public void handle(ActionEvent t) {
+	            	exitClick();
+	            }
+
+				private void exitClick() {
+					
+				}
+	        });
+	        themes.setOnAction(new EventHandler<ActionEvent>() {
+	            public void handle(ActionEvent t) {
+	            	themesClick();
+	            }
+				private void themesClick() {
+					
+				}
+	        });
 	        //adds the drop down option to the help tab(navHelp)
 	        navHelp.getItems().addAll(about);
+	        navFile.getItems().addAll(open, save, saveAs, exit);
+	        navEdit.getItems().addAll(themes);
+	        navOptions.getItems().addAll();
 	        
 	        //add nav tabs to navbar
-	        navBar.getMenus().addAll(navThemes, navEdit, navHelp);
+	        navBar.getMenus().addAll(navFile, navEdit, navOptions, navHelp);
 	        
 	        BorderPane border = new BorderPane();
 	        border.setTop(navBar);
