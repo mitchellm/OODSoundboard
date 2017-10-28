77d0b9207107d068b36f054782504ce739ea5cae
dmenear <davidlmenear@gmail.com>
2017-10-28 17:27:37 -0400

    Add progress bar sliders

--- Soundboard.java
+++ Soundboard.java
@@ -9,15 +9,18 @@ import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.event.ActionEvent;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.VBox;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
+import javafx.scene.transform.Rotate;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
@@ -148,11 +151,25 @@ public class Soundboard extends Application {
 	    buttonHB.setAlignment(Pos.CENTER);
 	    buttonHB.getChildren().addAll(btnPlay,btnPause,btnStop);
 	    
-	    //Create sliders
+	    /* ---START SLIDERS--- */
+	    double sliderWidth = 450;
+	    double volInitial = 50;
 		sldVol = sliderMaker.createVSlider();
+		sldVol.setValue(volInitial);
+		ProgressBar prgVol = new ProgressBar(volInitial/sldVol.getMax());
+		prgVol.setMinWidth(sliderWidth);
+		prgVol.setMaxWidth(sliderWidth);
+		prgVol.getTransforms().addAll(new Rotate(-90, 0, 0));
+		prgVol.setStyle("-fx-accent:blue");
+		prgVol.setPadding(new Insets(0, 7, 30, 7));
+		Group grpVol = new Group();
+		grpVol.getChildren().add(prgVol);
+		StackPane spVol = new StackPane();
+		spVol.getChildren().addAll(grpVol, sldVol);
 		sldVol.valueProperty().addListener((new ChangeListener<Number>() {
             public void changed(ObservableValue<? extends Number> ov,
                 Number old_val, Number new_val) {
+            	prgVol.setProgress(new_val.doubleValue()/100);
             	try {
             		mp.setVolume((double)new_val/100.0);
 	            } catch (NullPointerException e1) {
@@ -161,25 +178,47 @@ public class Soundboard extends Application {
             }
         }));
 		sldFreq = sliderMaker.createFSlider();
-		sldVol.setValue(50);
+		ProgressBar prgFreq = new ProgressBar(0);
+		prgFreq.setMinWidth(sliderWidth);
+		prgFreq.setMaxWidth(sliderWidth);
+		prgFreq.getTransforms().addAll(new Rotate(-90, 0, 0));
+		prgFreq.setPadding(new Insets(0, 7, 0, 7));
+		prgFreq.setStyle("-fx-accent:blue");
+		Group grpFreq = new Group();
+		grpFreq.getChildren().add(prgFreq);
+		StackPane spFreq = new StackPane();
+		spFreq.getChildren().addAll(grpFreq, sldFreq);
+		sldFreq.valueProperty().addListener((new ChangeListener<Number>() {
+            public void changed(ObservableValue<? extends Number> ov,
+                Number old_val, Number new_val) {
+            	prgFreq.setProgress(new_val.doubleValue()/19980);
+            }
+        }));
 		
 		//Create labels
 		lblVol = labelMaker.createVLabel();
-		lblVol.textProperty().bind(Bindings.format("%.0f", sldVol.valueProperty()));		
+		lblVol.textProperty().bind(Bindings.format("%.0f", sldVol.valueProperty()));	
+		lblVol.setPadding(new Insets(0, 30, 0, 0));
 		
 		lblFreq = labelMaker.createFLabel();
 		lblFreq.textProperty().bind(Bindings.format("%.0f", sldFreq.valueProperty()));
 
 		//Create containers for holding sliders and labels		
+		double vbWidth = 65;
+		
 		VBox vSliderVB = new VBox();
-		vSliderVB.getChildren().addAll(sldVol, lblVol);
+		vSliderVB.getChildren().addAll(spVol, lblVol);
 		vSliderVB.setSpacing(10);
 		vSliderVB.setAlignment(Pos.CENTER);
+		vSliderVB.setMinWidth(vbWidth);
+		vSliderVB.setMaxWidth(vbWidth);
 		
 		VBox fSliderVB = new VBox();
-		fSliderVB.getChildren().addAll(sldFreq, lblFreq);
+		fSliderVB.getChildren().addAll(spFreq, lblFreq);
 		fSliderVB.setSpacing(10);
 		fSliderVB.setAlignment(Pos.CENTER);
+		fSliderVB.setMinWidth(vbWidth);
+		fSliderVB.setMaxWidth(vbWidth);
 		
 		//Create HBox for holding slider containers
 		HBox sliderHB = new HBox();
@@ -187,6 +226,7 @@ public class Soundboard extends Application {
 		sliderHB.setPadding(new Insets(10, 50, 50, 50));
 		sliderHB.setSpacing(40);
 		sliderHB.setAlignment(Pos.CENTER);
+		/* ---END SLIDERS--- */
 		
 		//Create primary borderpane for layout and add sections
 		BorderPane root = new BorderPane();
@@ -195,7 +235,9 @@ public class Soundboard extends Application {
 		root.setBottom(buttonHB);
 		
 		//Build stage
-		primaryStage.setScene(new Scene(root, 1000, 700));
+		Scene primaryScene = new Scene(root, 1000, 700);
+		primaryScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+		primaryStage.setScene(primaryScene);
 		primaryStage.setTitle("Project Test");
 		primaryStage.setResizable(false);
 		primaryStage.show();
